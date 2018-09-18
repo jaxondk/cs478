412923719044ee064649a8fcf1bf767a0bef14a8
Jaxon Keeler <jaxon@artifylabs.com>
2018-09-18 12:16:30 -0600

    model works for linearlySeparable.arff

--- toolkitPython/toolkit/perceptron_learner.py
+++ toolkitPython/toolkit/perceptron_learner.py
@@ -2,20 +2,45 @@ from __future__ import (absolute_import, division, print_function, unicode_liter
 
 from .supervised_learner import SupervisedLearner
 from .matrix import Matrix
+import numpy as np
 
 
 class PerceptronLearner(SupervisedLearner):
-    """
-    For nominal labels, this model simply returns the majority class. For
-    continuous labels, it returns the mean value.
-    If the learning model you're using doesn't do as well as this one,
-    it's time to find a new learning model.
-    """
-
+    MAX_EPOCHS = 25 
+    STALL_NUM_EPOCHS = 5
+    LEARNING_RATE = .1
     labels = []
+    weights = []
 
     def __init__(self):
         pass
+
+    def initWeights(self, numFeatures):
+        self.weights = np.zeros(numFeatures+1) #+1 for bias weight
+        print('Initial weights: ', self.weights)
+
+    # Change in weights = c(t-z)x_i
+    def updateWeights(self, pattern, label, out):
+        change = self.LEARNING_RATE * (label - out) * pattern
+        print('Change in weights: ', change)
+        self.weights += change
+        print('Updated weights: ', self.weights)
+
+    # Sum(w_i*x_i)
+    def calcNet(self, pattern):
+        net = (self.weights * pattern).sum()
+        print("Net: ", net)
+        return net
+
+    def trainModel(self, instance, label):
+        pattern = np.array(instance)
+        pattern = np.append(pattern, 1) #include a bias
+        print('Pattern + bias: ', pattern)
+        net = self.calcNet(pattern)
+        # Fire perceptron?
+        out = 1.0 if (net > 0) else 0.0
+        self.labels.append(out)
+        self.updateWeights(pattern, label, out)
 
     def train(self, features, labels):
         """
@@ -23,11 +48,19 @@ class PerceptronLearner(SupervisedLearner):
         :type labels: Matrix
         """
         self.labels = []
-        for i in range(labels.cols):
-            if labels.value_count(i) == 0:
-                self.labels += [labels.column_mean(i)]          # continuous
-            else:
-                self.labels += [labels.most_common_value(i)]    # nominal
+        self.initWeights(features.cols)
+        # labels.print()
+        # print('-------------------------')
+        # features.print()
+
+        # Train over several epochs. Remember, an epoch is an iteration over the whole training set
+        for e in range(self.MAX_EPOCHS):
+            print("Epoch " + str(e))
+            # Should not need to iterate through instances due to broadcasting?... Not sure how though
+            for i in range(features.rows):
+                self.trainModel(features.row(i), labels.row(i)[0])
+
+            # TODO: check accuracy. If accuracy stalls for STALL_NUM_EPOCHS, break
 
     def predict(self, features, labels):
         """
