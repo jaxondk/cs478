5d8141fb7127e13ffc1063393d80b22ac757c8ff
Jaxon Keeler <jaxon@artifylabs.com>
2018-11-03 18:19:58 -0600

    pseudo code, example dataset

--- toolkitPython/toolkit/decision_tree_learner.py
+++ toolkitPython/toolkit/decision_tree_learner.py
@@ -6,9 +6,48 @@ from .matrix import Matrix
 import numpy as np
 import matplotlib.pyplot as plt
 
+class Node():
+  name = None # string
+  features = [] # Matrix. Features of this node's instance set
+  labels = [] # Array. Labels of this node's instance set
+  parent = None # Node
+  children = [] # Node []
+  out = None 
+
+  def __init__(self, name, features, labels, parent):
+    self.name = name
+    self.parent = parent
+
+  def addChild(self, child):
+    self.children.append(child)
+
+  ### Check for leaf node. If all the labels are equal for the current set, then this is a leaf node
+  def isLeafNode(self):
+    firstLabel = self.labels[0]
+    if(all(label == firstLabel for label in self.labels[1:])):
+      print(self.name + ' is a leaf node')
+      return True
+    else:
+      return False
+
+  # TODO
+  def noMoreAttributes(self):
+    pass
+
+  # Recursive algorithm.
+  def id3(self):
+    if (self.isLeafNode() or self.noMoreAttributes()):
+      return
+
+    ### Calc entropy for entire set at c
+    ### For all attributes available at c:
+        ### Calc entropy of attribute
+        ### Calc info gain of attribute
+    ### Choose attribute A with highest info gain. Split on A’s possible values
+    ### Make c = next node from A’s possible values
 
 class DecisionTreeLearner(SupervisedLearner):
-    
+    root = None
 
     def __init__(self):
         pass
@@ -18,7 +57,8 @@ class DecisionTreeLearner(SupervisedLearner):
         :type features: Matrix
         :type labels: Matrix
         """
-        self.id3(features, labels)
+        self.root = Node('root', features, labels.col(0), None)
+        self.root.id3()
 
     def predict(self, features, labels):
         """
