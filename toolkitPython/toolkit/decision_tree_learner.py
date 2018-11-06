847804403d79878248a2522e22229b93b3b49226
Jaxon Keeler <jaxon@artifylabs.com>
2018-11-06 16:49:34 -0700

    working

--- toolkitPython/toolkit/decision_tree_learner.py
+++ toolkitPython/toolkit/decision_tree_learner.py
@@ -7,15 +7,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Node():
-  name = None # string
-  attrForSplit = None # index
-  avFromSplit = None # attribute value for split
-  instances = None # Matrix. Features of this node's instance set
-  labels = None # Matrix. Labels of this node's instance set
-  parent = None # Node
-  children = None # Node dict. key=attribute value for the split, value=Node
-  availableAttributes = None # Array of column indices. These features have not been split on yet
-  out = None 
+  ###### NOTE: these are CLASS variables. they are different than self.VARNAME.
+  ######       Commenting them out fixed a recursion error when using cross validation
+  # name = None # string
+  # attrForSplit = None # index
+  # avFromSplit = None # attribute value for split
+  # instances = None # Matrix. Features of this node's instance set
+  # labels = None # Matrix. Labels of this node's instance set
+  # parent = None # Node
+  # children = None # Node dict. key=attribute value for the split, value=Node
+  # availableAttributes = None # Array of column indices. These features have not been split on yet
+  # out = None 
 
   def print(self):
     print('----- Node: {0} -----'.format(self.name))
@@ -30,8 +32,8 @@ class Node():
     self.instances = instances
     self.labels = labels
     self.parent = parent
-    self.availableAttributes = availableAttributes
     self.children = {}
+    self.availableAttributes = availableAttributes
 
   def split(self, attrForSplit):
     self.attrForSplit = attrForSplit
@@ -57,38 +59,35 @@ class Node():
     return len(self.availableAttributes) == 0
 
   # Calc entropy for entire set at current node
-  def calcEntropySet(self, labels):
-    classes = list(set(labels))
-    entropy = 0
-    for i in classes:
-      p = labels.count(i)/len(labels)
-      entropy -= p * np.log2(p)
-    return entropy
+  # def calcEntropySet(self, labels):
+  #   classes = list(set(labels))
+  #   entropy = 0
+  #   for i in classes:
+  #     p = labels.count(i)/len(labels)
+  #     entropy -= p * np.log2(p)
+  #   return entropy
 
   def calcInfoS_a(self, a, vc, attr_counts, label_counts): 
     denominator = attr_counts[a, :vc]
     # print('denom:', denominator)
-    # print('denom shape', denominator.shape)
     numerators = []
     for av in range(vc):
       numerators.append(label_counts[:, a][:vc, av])
     numerators = np.array(numerators)
     # print('numers:', numerators)
-    # print('numers shape', numerators.shape)
     p = numerators / denominator[:, None] #this allows you to correctly broadcast denom, even though it is (valCount x null) in shape
     unsummed = np.where(p > 0, p * np.log2(p), 0) #Note: where still runs the np.log part even if p>0, just doesn't return it. So still get warning
-    ret = -(unsummed).sum(axis=1)
-    # print('ret', ret)
-    return ret
+    # print('unsummed', unsummed)
+    return -(unsummed).sum(axis=1)
 
   def calcEntropyAttributes(self):
     entropy = 0
     attrs = range(self.instances.cols)
     entropy_attrs = np.zeros(len(attrs))
     maxAttr = self.instances.maxValueCount()
-    # This will be jagged array of nAttr x (Val count of attr). This is Si in the formula
+    # This will be  array of nAttr x (maxValCount of attrs). This is Si in the formula
     attr_counts = np.zeros((len(attrs), maxAttr))
-    # This is an array of jagged arrays (of same shape as attr_counts). This is numerator of pi of Info(Si). There is an array of jagged arrays for every possible label
+    # This is an array of arrays (of same shape as attr_counts). This is numerator of pi of Info(Si). There is an array of arrays for every possible label
     label_counts = np.zeros((self.labels.value_count(0), len(attrs), maxAttr))
     
     ### Run through instances and count how many of each attribute value there is
@@ -101,14 +100,22 @@ class Node():
     for a in attrs:
       vc = self.instances.value_count(a) # all the vc indexing stuff is to take care of garbage columns that were added to avoid jagged arrays
       fraction = (attr_counts[a, :vc] / self.instances.rows)
-      entropy_attrs[a] = np.sum(fraction * self.calcInfoS_a(a, vc, attr_counts, label_counts))
+      infoS_a = self.calcInfoS_a(a, vc, attr_counts, label_counts)
+      # print('fraction', fraction)
+      # print('infoS_a', infoS_a)
+      # input('pause')
+      entropy_attrs[a] = np.sum(fraction * infoS_a)
     return entropy_attrs
 
   # Recursive algorithm.
   def id3(self):
-    self.print()
-    # input('pause')
-    if (self.isPureLeafNode()):
+    # self.print()
+    # input('pause'))
+    if(self.instances.rows == 0):
+      self.out = max(self.parent.labels.col(0), key=self.labels.col(0).count) # returns mode of array
+      print('No instances for {0}. Out={1}'.format(self.name, self.out))
+      return
+    elif (self.isPureLeafNode()):
       self.out = self.labels.get(0, 0)
       print('{0} is a leaf node. Out={1}'.format(self.name, self.out))
       return
@@ -117,21 +124,22 @@ class Node():
       print('{0} has no more attributes to split on. Out={1}'.format(self.name, self.out))
       return
     entropy_attrs = self.calcEntropyAttributes()
-    print('entropy of attrs\n', entropy_attrs)
+    # print('entropy of attrs\n', entropy_attrs)
+    # input('pause')
     attrForSplit = np.argmin(entropy_attrs)
-    print('Split on {0}'.format(self.instances.attr_name(attrForSplit)))
+    # print('Split on {0}'.format(self.instances.attr_name(attrForSplit)))
     self.split(attrForSplit)
     ### Make current node = next node from A’s possible values. Do this in loop so that when one node is done doing id3, continues with sibling
     for child in self.children.values():
       child.id3()
-    print('Done with node {0}'.format(self.name))
+    # print('Done with node {0}'.format(self.name))
 
   # Recursive Function
   def predict(self, instance):
-    print('Child for prediction: {0}'.format(self.name))
+    # print('Child for prediction: {0}'.format(self.name))
     # if no children, return self.out as prediction
     if(len(self.children) == 0):
-      print('{0} has no children, returning {1} for prediction'.format(self.name, self.out))
+      # print('{0} has no children, returning {1} for prediction'.format(self.name, self.out))
       return self.out
     # grab the attribute value for attrForSplit from instance
     av = instance[self.attrForSplit]
@@ -160,7 +168,7 @@ class DecisionTreeLearner(SupervisedLearner):
         """
         del labels[:]
         label = self.root.predict(instance)
-        print('Label: ', label)
+        # print('Label: ', label)
         labels += [label]
 
         
